5c79da022b710451cd9a452e97b0524fb44848a8
saaranshbhoj <bhojsaaransh@gmail.com>
2025-01-10 23:22:38 +0530

    episode-01

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-const heading=React.createElement("h1",{},"Hello React!!")
+const heading=React.createElement("h1",{},"This is Hello World from react!!");
+console.log(heading)
+const root=ReactDOM.createRoot(document.getElementById('root'));
+// to create h1 tag inside <div> tag
+
+const heading1=React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},[
+    React.createElement("h1",{},'this is h1 tag inside nested <div>tags'),
+    React.createElement("h2",{},'this is second h2 tag inside <div>tag')]
+     )
+);
+const root1=ReactDOM.createRoot(document.getElementById('root'));
+root1.render(heading1);
