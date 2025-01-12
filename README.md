49508736d974ead5fc3940f38b112e4d3349b16b
saaranshbhoj <bhojsaaransh@gmail.com>
2025-01-12 22:54:45 +0530

    episode-02

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 #Namaste React Hello React!!
+
+#Parcel gives us good-visual error!
+Created local server
+created dev build 
+doing HMR Hot Module Replacement
+uses a file watching algorithm-written in c++
+faster dev exp by caching 
+Image Optimization
+Minification of the file(it is basically a bundler bundle files together to make them ready for production!)
--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const heading=React.createElement("h1",{},"This is Hello World from react!!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading=React.createElement("h1",{},"This is Magic due to parcel!!");
 console.log(heading)
 const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading)
 // to create h1 tag inside <div> tag
 
-const heading1=React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"},[
-    React.createElement("h1",{},'this is h1 tag inside nested <div>tags'),
-    React.createElement("h2",{},'this is second h2 tag inside <div>tag')]
-     )
-);
-const root1=ReactDOM.createRoot(document.getElementById('root'));
-root1.render(heading1);
+// const heading1=React.createElement("div",{id:"parent"},
+//     React.createElement("div",{id:"child"},[
+//     React.createElement("h1",{},'this is h1 tag inside nested <div>tags'),
+//     React.createElement("h2",{},'this is second h2 tag inside <div>tag')]
+//      )
+// );
+// //const root1=ReactDOM.createRoot(document.getElementById('root'));
+// //root1.render(heading1);
