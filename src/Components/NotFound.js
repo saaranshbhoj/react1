5f97253ccd72959b3f56499479196e1936181643
saaranshbhoj <bhojsaaransh@gmail.com>
2025-02-02 20:54:53 +0530

    Updated with Tailwind-Css

---
+++ src/Components/NotFound.js
@@ -0,0 +1,9 @@
+//If no route matches to the current url;
+const NotFound=() =>{
+    return(
+        <div className="notfound">
+            <h1>This Component will be displayed when No Routes Path matches to the current URL Address!</h1>
+        </div>
+    )
+}
+export default NotFound;
